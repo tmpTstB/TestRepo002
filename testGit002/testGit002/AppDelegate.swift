585d5f3f4bd6bf823a01b12b55879d0402f25942
tmpTstB <tsinfob@gmail.com>
2014-09-27 21:27:29 +0300

    testing conflict 3 (Git-Hub)

--- testGit002/testGit002/AppDelegate.swift
+++ testGit002/testGit002/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     //var window: UIWindow?
-    //testVar=2
+    //testVar=4
 
     //func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
